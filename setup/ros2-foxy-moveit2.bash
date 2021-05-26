7a7fbc3ea51b6ac3cf1237d4b9f51f45110ed662
Nisala Kalupahana <avishkank@gmail.com>
2021-05-26 00:21:10 -0500

    Update ros2-foxy-moveit2.bash

--- setup/ros2-foxy-moveit2.bash
+++ setup/ros2-foxy-moveit2.bash
@@ -1,2 +1,5 @@
-brew install octomap qhull bullet
+brew install octomap qhull
 ln -s /usr/local/opt/qt@5 /usr/local/opt/qt
+git clone https://github.com/bulletphysics/bullet3.git
+cd bullet3
+./build_cmake_pybullet_double.sh
