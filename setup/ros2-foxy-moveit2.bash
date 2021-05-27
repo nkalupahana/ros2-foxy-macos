002ee3f413ab73734bc6d74703269bc49efb0652
Nisala Kalupahana <avishkank@gmail.com>
2021-05-27 13:17:33 -0500

    Update ros2-foxy-moveit2.bash

--- setup/ros2-foxy-moveit2.bash
+++ setup/ros2-foxy-moveit2.bash
@@ -1,2 +1,3 @@
 brew install octomap qhull fcl boost-python3 libomp freeglut
 ln -s /usr/local/opt/qt@5 /usr/local/opt/qt
+sed -i -e 's/FGAPI GLUTproc FGAPIENTRY glutGetProcAddress/\/\/FGAPI GLUTproc FGAPIENTRY glutGetProcAddress/g' $(realpath /usr/local/include/GL/freeglut_ext.h)
