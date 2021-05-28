45b94dbfd7dfd4e522f4be18890047668cf2126f
Nisala Kalupahana <avishkank@gmail.com>
2021-05-28 00:37:15 -0500

    Update ros2-foxy-nav2.bash

--- setup/ros2-foxy-nav2.bash
+++ setup/ros2-foxy-nav2.bash
@@ -1,2 +1,2 @@
-brew install ompl ossp-uuid graphicsmagick suite-sparse libomp ode osrf/simulation/gazebo10
+brew install ompl ossp-uuid graphicsmagick suite-sparse libomp ode tbb@2020 osrf/simulation/gazebo11
 echo "LIBRARY_PATH=/usr/local/opt/ode/lib/" >> $GITHUB_ENV
