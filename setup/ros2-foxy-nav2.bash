dc3c99d3bd67be4307d4b7b1753200fc37356854
Nisala Kalupahana <avishkank@gmail.com>
2021-05-28 10:33:34 -0500

    Update ros2-foxy-nav2.bash

--- setup/ros2-foxy-nav2.bash
+++ setup/ros2-foxy-nav2.bash
@@ -1,2 +1,4 @@
 brew install ompl ossp-uuid graphicsmagick suite-sparse libomp ode tbb@2020 osrf/simulation/gazebo11
-echo "LIBRARY_PATH=/usr/local/opt/ode/lib/" >> $GITHUB_ENV
+export "LIBRARY_PATH=/usr/local/opt/tbb@2020_u3/lib:/usr/local/opt/ode/lib/:$LIBRARY_PATH"
+export CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}:/usr/local/opt/tbb@2020_u3
+export CPATH=${CPATH}:/usr/local/opt/tbb@2020_u3/include
