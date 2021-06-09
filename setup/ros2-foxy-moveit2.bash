2689a63210822c813783dbdfa8a9618d46824522
Nisala Kalupahana <avishkank@gmail.com>
2021-06-09 14:38:14 -0500

    Update ros2-foxy-moveit2.bash

--- setup/ros2-foxy-moveit2.bash
+++ setup/ros2-foxy-moveit2.bash
@@ -2,5 +2,4 @@ brew install octomap qhull fcl boost-python3 libomp freeglut coreutils glew ompl
 ln -s /usr/local/opt/qt@5 /usr/local/opt/qt
 ln -s /usr/local/opt/openssl/include/openssl /usr/local/include
 export LIBRARY_PATH=/usr/local/opt/ode/lib/:$LIBRARY_PATH
-cd /usr/local/include/openssl
-ls
+export PACKAGES_SKIP=moveit_servo
