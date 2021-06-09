c9ae76bd2f6b1fa1f95fd2d6749233e0d8f375bf
Nisala Kalupahana <avishkank@gmail.com>
2021-06-09 02:07:07 -0500

    Update ros2-foxy-moveit2.bash

--- setup/ros2-foxy-moveit2.bash
+++ setup/ros2-foxy-moveit2.bash
@@ -1,2 +1,3 @@
-brew install octomap qhull fcl boost-python3 libomp freeglut coreutils glew ompl
+brew install octomap qhull fcl boost-python3 libomp freeglut coreutils glew ompl ode
 ln -s /usr/local/opt/qt@5 /usr/local/opt/qt
+export LIBRARY_PATH=/usr/local/opt/ode/lib/:$LIBRARY_PATH
